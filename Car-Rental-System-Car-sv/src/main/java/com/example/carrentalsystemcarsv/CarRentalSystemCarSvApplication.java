55245b4c7c502abe975874676639ae8400aecaba
VinulVinsura <155805055+VinulVinsura@users.noreply.github.com>
2024-06-07 22:55:36 +0530

    creat car-sv

---
+++ Car-Rental-System-Car-sv/src/main/java/com/example/carrentalsystemcarsv/CarRentalSystemCarSvApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalsystemcarsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemCarSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemCarSvApplication.class, args);
+	}
+
+}
---
+++ Car-Rental-System-Car-sv/src/test/java/com/example/carrentalsystemcarsv/CarRentalSystemCarSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentalsystemcarsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemCarSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
